591a0d7a4032d3a7f45c6394211af9430e5b56e0
agei5poli5 <willburghard@gmail.com>
2020-04-20 14:46:37 -0400

    Updates

---
+++ precache-manifest.3f2a4abc1d10f3ea1fbfea9e3b3567a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235a6a013752370a5505b6e649cfc015",
+    "url": "/Jammming/index.html"
+  },
+  {
+    "revision": "25193baa609070ac9a47",
+    "url": "/Jammming/static/css/main.c088b2dd.chunk.css"
+  },
+  {
+    "revision": "9010e8ed492a93db1b58",
+    "url": "/Jammming/static/js/2.55ec0149.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jammming/static/js/2.55ec0149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25193baa609070ac9a47",
+    "url": "/Jammming/static/js/main.6b7bc832.chunk.js"
+  },
+  {
+    "revision": "11ac3fdaeaba5d66b9d2",
+    "url": "/Jammming/static/js/runtime-main.504eab9c.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/Jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
